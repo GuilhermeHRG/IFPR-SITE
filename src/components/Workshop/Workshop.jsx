63ef510814f46eeb23a4ed1fc86f25fa20f71885
Guilherme Guelere <guilhermeg.dev@gmail.com>
2024-04-23 23:25:46 -0300

    Update

--- src/components/Workshop/Workshop.jsx
+++ src/components/Workshop/Workshop.jsx
@@ -13,21 +13,22 @@ function Workshop() {
                <img src="https://img.freepik.com/vetores-gratis/padrao-ciencia-rabisco_23-2147492328.jpg?t=st=1713717059~exp=1713720659~hmac=5f2fd2a1859b160385b750f88d96bdf7644ff4a27bb4670ce81fedb7127a2042&w=740" alt="imagem" />
                <div className="descWork">
                   <h3>Oficina 1</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo labore, earum rem sint voluptatem assumenda perferendis dolorem cupiditate voluptates quo?
+                  </p>
                </div>
             </div>
             <div className="card2">
                <img src="https://img.freepik.com/vetores-gratis/padrao-ciencia-rabisco_23-2147492328.jpg?t=st=1713717059~exp=1713720659~hmac=5f2fd2a1859b160385b750f88d96bdf7644ff4a27bb4670ce81fedb7127a2042&w=740" alt="imagem" />
                <div className="descWork">
                   <h3>Oficina 1</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste fuga odit aut nisi error dolores? Et exercitationem fugit consectetur eum.</p>
                </div>
             </div>
             <div className="card2">
                <img src="https://img.freepik.com/vetores-gratis/padrao-ciencia-rabisco_23-2147492328.jpg?t=st=1713717059~exp=1713720659~hmac=5f2fd2a1859b160385b750f88d96bdf7644ff4a27bb4670ce81fedb7127a2042&w=740" alt="imagem" />
                <div className="descWork">
                   <h3>Oficina 1</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos velit reiciendis amet error. Quibusdam dolorum modi dolore ea dignissimos!</p>
                </div>
             </div>
          </div>
